f798e23af9832a0bdb68fe9b68d7d403f48e8ec5
JAKUB OCZKO <JAKUB.OCZKO@ing.com>
2022-04-18 20:18:19 +0200

    Homework1

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,24 @@
+﻿while (true)
+{
+    Console.WriteLine("Please enter any keyboard key");
+    char key = Console.ReadKey(true).KeyChar;
+
+    if (char.ToLower(key) == 'q')
+    {
+        Console.WriteLine("The exit key has been pressed");
+        break;
+    }
+    else if (char.IsDigit(key))
+    {
+        Console.WriteLine($"The number was pressed");
+        Console.WriteLine($"{key} in ASCII = {(int)key}");
+    }
+    else if (char.IsControl(key))
+    {
+        Console.WriteLine($"The function key was pressed");
+    }
+    else
+    {
+        Console.WriteLine($"{key} in ASCII = {(int)key}");
+    }
+}
